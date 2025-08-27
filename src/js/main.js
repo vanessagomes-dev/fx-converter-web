ed7760449c745b71f12f78fcfc2a7fb0dec9e7fb
vanessagomesdev <vanessagomesdev@gmail.com>
2025-08-27 12:39:53 -0300

    feat(js): formatação da moeda e exibição

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
 const footer = document.querySelector("main footer");
+const description = document.getElementById("description")
 
 //Manipulando o input amount para receber somente números
 amount.addEventListener("input", () => {
@@ -35,12 +36,13 @@ form.onsubmit = (event) => {
 //Função para converter a moeda
 function convertCurrency(amount, price, symbol) {
   try {
+    description.textContent = `${symbol} 1 = ${price}`
     //Aplica a classe que exibe o footer mostrando o resultado
     footer.classList.add("show-result");
   } catch (error) {
     // remove a classe removendo ele da tela
     footer.classList.remove("show-result");
-    
+
     console.log(error);
     alert("Não foi possível converter.Tente novamente mais tarde.");
   }
