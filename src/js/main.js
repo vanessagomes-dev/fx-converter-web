244be7184e5eee4a43a6fca0cec63f2e5127a070
vanessagomesdev <vanessagomesdev@gmail.com>
2025-08-27 11:50:46 -0300

    feat(js): manipulando o input amount para receber somente números

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+const amount = document.getElementById("amount")
+
+//Manipulando o input amount para receber somente números
+amount.addEventListener("input", () =>{
+    const hasCharactersRegex = /\D+/g
+    amount.value = amount.value.replace(hasCharactersRegex, "")    
+})
