8e07d2a9cd3e0b952fb631394eeec7fddc1968fc
vanessagomesdev <vanessagomesdev@gmail.com>
2025-08-27 12:19:51 -0300

    feat(js): função para identificar a moeda

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,37 @@
-const form = document.querySelector("form")
-const amount = document.getElementById("amount")
-const currency = document.getElementById("currency")
+// Cotação de moedas do dia
+const USD = 4.87;
+const EUR = 5.32;
+const GBP = 6.08;
+
+//Obtendo os elementos do formulário
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
 
 //Manipulando o input amount para receber somente números
-amount.addEventListener("input", () =>{
-    const hasCharactersRegex = /\D+/g
-    amount.value = amount.value.replace(hasCharactersRegex, "")    
-})
+amount.addEventListener("input", () => {
+  const hasCharactersRegex = /\D+/g;
+  amount.value = amount.value.replace(hasCharactersRegex, "");
+});
 
 // Captando o evento de submit do formulário
 form.onsubmit = (event) => {
-    event.preventDefault ()
+  event.preventDefault();
+
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$");
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€");
+      break;
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£");
+      break;
+  }
+};
+
+//Função para converter a moeda
+function convertCurrency(amount, price, symbol) {
+    
 }
