b99c56ba8c6396fbe373f3245a1845e462f78108
vanessagomesdev <vanessagomesdev@gmail.com>
2025-08-27 12:01:00 -0300

    feat(js): Obtendo a moeda selecionada pelo usuario. Captando o evento do formulário.

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,14 @@
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 //Manipulando o input amount para receber somente números
 amount.addEventListener("input", () =>{
     const hasCharactersRegex = /\D+/g
     amount.value = amount.value.replace(hasCharactersRegex, "")    
 })
+
+// Captando o evento de submit do formulário
+form.onsubmit = (event) => {
+    event.preventDefault ()
+}
